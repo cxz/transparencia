a455bb578b0751cc37f09212f72768e88eda5585
amaia <alexandre.maia@gmail.com>
2018-03-10 00:26:38 -0300

    fix indentation

--- static/js/chart.js
+++ static/js/chart.js
@@ -8,30 +8,30 @@ function makeGraphs(error, recordsJson) {
     var enquadramento = "Enquadramento do Processo";
     var situacao = "Situação";
     var valorContrato = "Valor do contrato";
-    
-	var records = recordsJson;
-	var dateFormat = d3.time.format("%d/%m/%Y");
-    	
-	records.forEach(function(d) {
+
+    var records = recordsJson;
+    var dateFormat = d3.time.format("%d/%m/%Y");
+
+    records.forEach(function(d) {
         d[inicioVigencia] = dateFormat.parse(d[inicioVigencia]);
-		d[inicioVigencia].setMinutes(0);
-		d[inicioVigencia].setSeconds(0);
-	});    
+        d[inicioVigencia].setMinutes(0);
+        d[inicioVigencia].setSeconds(0);
+    });    
 
     //Create a Crossfilter instance
-	var ndx = crossfilter(records);
+    var ndx = crossfilter(records);
 
-	//Define Dimensions
-	var dateDim = ndx.dimension(function(d) { return d[inicioVigencia]; });
+    //Define Dimensions
+    var dateDim = ndx.dimension(function(d) { return d[inicioVigencia]; });
     var fornecedorDim = ndx.dimension(function(d) { return d[fornecedor]; });
     var enquadramentoDim = ndx.dimension(function(d) { return d[enquadramento]; });
     var situacaoDim = ndx.dimension(function(d) { return d[situacao]; });
     var valoresDim = ndx.dimension(function(d) { return d[fornecedor] });
-    
-	var allDim = ndx.dimension(function(d) {return d;});
 
-	//Group Data
-	var numRecordsByDate = dateDim.group();
+    var allDim = ndx.dimension(function(d) {return d;});
+
+    //Group Data
+    var numRecordsByDate = dateDim.group();
     var fornecedorGroup = fornecedorDim.group();
     var enquadramentoGroup = enquadramentoDim.group();
     var situacaoGroup = situacaoDim.group();
@@ -39,27 +39,27 @@ function makeGraphs(error, recordsJson) {
 
     var all = ndx.groupAll();
 
-	//Define values (to be used in charts)
-	var minDate = dateDim.bottom(1)[0][inicioVigencia];
-	var maxDate = dateDim.top(1)[0][inicioVigencia];
+    //Define values (to be used in charts)
+    var minDate = dateDim.bottom(1)[0][inicioVigencia];
+    var maxDate = dateDim.top(1)[0][inicioVigencia];
 
     //Charts
     var timeChart = dc.barChart("#time-chart");
     var fornecedorChart = dc.rowChart("#fornecedor-row-chart");
     var enquadramentoChart = dc.pieChart("#enquadramento-pie-chart");
-            
-	timeChart
-		.width(600)
-		.height(140)
-		//.margins({top: 10, right: 50, bottom: 20, left: 20})
-		.dimension(dateDim)
-		.group(numRecordsByDate)
-		.transitionDuration(500)
-		.x(d3.time.scale().domain([minDate, maxDate]))
-		.elasticY(true)
-		.yAxis().ticks(4);
-    
-	fornecedorChart
+
+    timeChart
+        .width(600)
+        .height(140)
+        //.margins({top: 10, right: 50, bottom: 20, left: 20})
+        .dimension(dateDim)
+        .group(numRecordsByDate)
+        .transitionDuration(500)
+        .x(d3.time.scale().domain([minDate, maxDate]))
+        .elasticY(true)
+        .yAxis().ticks(4);
+
+    fornecedorChart
         .width(600)
         .height(500)
         .dimension(fornecedorDim)
@@ -68,7 +68,7 @@ function makeGraphs(error, recordsJson) {
         .elasticX(true)
         .cap(20)
         ;
-    
+
     enquadramentoChart
         .width(300)
         .height(100)
@@ -77,7 +77,7 @@ function makeGraphs(error, recordsJson) {
         .externalLabels(-50)        
         .legend(dc.legend().x(0))
         ;
-    
+
     var situacaoChart = dc.pieChart("#situacao-pie-chart")
         .width(300)
         .height(100)
@@ -86,7 +86,7 @@ function makeGraphs(error, recordsJson) {
         .externalLabels(-50)        
         .legend(dc.legend().x(0))    
         ;
-    
+
     var dataTable = dc
         .dataTable("#valores-table")    
         .width(300)
@@ -100,7 +100,7 @@ function makeGraphs(error, recordsJson) {
             { label: situacao, format: function(d) { return d[situacao]; } },
          ])
         ;    
-        
-	dc.renderAll();
+
+    dc.renderAll();
 
 };
